7aa4102c1c18120b3f0fe9519f887fa76e626893
Brennan Cheung <git@brennancheung.com>
2020-01-23 15:12:00 -0800

    adding checks api

--- brigade.js
+++ brigade.js
@@ -2,23 +2,40 @@ const { events, Job } = require('brigadier')
 
 const registry = 'core.harbor.volgenic.com/ui'
 
-const runJob = (name, image, tasks = []) => {
+const sendCheckStatus = (e, summary = 'Beginning test run', conclusion, text) => {
+  // This Check Run image handles updating GitHub
+  const checkRunImage = 'brigadecore/brigade-github-check-run:latest'
+
+  const job = new Job('start-run', checkRunImage)
+  job.env = {
+    CHECK_PAYLOAD: e.payload,
+    CHECK_NAME: 'tests',
+    CHECK_TITLE: 'Tests',
+    CHECK_SUMMARY: summary,
+    CHECK_CONCLUSION: conclusion,
+    CHECK_TEXT: text,
+  }
+  return job.run()
+}
+
+const createJob = (name, image, tasks = []) => {
   const fullImage = `${registry}/${image}`
   const job = new Job(name, fullImage, tasks, true)
   job.streamLogs = true
   job.imagePullSecrets = ['regcred']
-  job.run()
+  job.imageForcePull = true
   return job
 }
 
-events.on('push', (e, project) => {
+events.on('check_suite:requested', (e, project) => {
   // Webhook event received
-  console.log(`Received push for commit ${e.revision.commit}`)
+  console.log(`Received check_suite for commit ${e.revision.commit}`)
 
   // Run unit tests
   console.log('About to run unit tests')
+  sendCheckStatus(e)
 
-  // Try to run in parallel
-  runJob('jest-runner', 'hello-service', ['yarn jest'])
-  runJob('jest-runner2', 'hello-service', ['yarn jest'])
+  createJob('jest-runner', 'hello-service', ['yarn jest']).run()
+    .then(result => sendCheckStatus(e, 'Tests passed', 'success', result.toString()))
+    .catch(err => sendCheckStatus(e, 'Tests failed', 'failure', err))
 })
