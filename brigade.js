92fddac1779d801a5ad3266452896861f2d60f7c
Brennan Cheung <git@brennancheung.com>
2020-01-22 19:17:44 -0800

    Add a space

--- brigade.js
+++ brigade.js
@@ -6,7 +6,7 @@ events.on('push', (e, project) => {
 
   // Run unit tests
   console.log('About to run unit tests')
-  var jest = newJob('jest-runner')
+  var jest = new Job('jest-runner')
   jest.image = "core.harbor.volgenic.com/ui/hello-service:latest"
   jest.tasks = ['yarn jest']
   jest.streamLogs = true
