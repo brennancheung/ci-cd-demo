a753bca3dda5bab2857c12fa2923800441d53da6
Brennan Cheung <git@brennancheung.com>
2020-01-22 19:16:14 -0800

    Run unit tests in brigade

--- brigade.js
+++ brigade.js
@@ -1,5 +1,15 @@
 const { events } = require('brigadier')
 
 events.on('push', (e, project) => {
+  // Webhook event received
   console.log(`Received push for commit ${e.revision.commit}`)
+
+  // Run unit tests
+  console.log('About to run unit tests')
+  var jest = newJob('jest-runner')
+  jest.image = "core.harbor.volgenic.com/ui/hello-service:latest"
+  jest.tasks = ['yarn jest']
+  jest.streamLogs = true
+  jest.imagePullSecrets = ['regcred']
+  jestRunner.run()
 })
