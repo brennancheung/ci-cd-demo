fd91f74faf94727e4851753be5f307a9bb07328e
Brennan Cheung <git@brennancheung.com>
2020-01-23 18:25:57 -0800

    moar parallel

--- brigade.js
+++ brigade.js
@@ -68,7 +68,14 @@ events.on('check_suite:requested', async (e, project) => {
   } 
 
   const lintJob = createJob('lint-runner', 'hello-service', ['yarn jest'])
-  const unitTestsJob = createJob('unit-runner', 'hello-service', ['yarn jest'])
   runTest('lint', 'Lint', lintJob)
+
+  const unitTestsJob = createJob('unit-runner', 'hello-service', ['yarn jest'])
   runTest('unit', 'Unit', unitTestsJob)
+
+  const maxParallelIntegration = 5
+  for (let i=1; i<=maxParallelIntegration; i++) {
+    const job = createJob(`cypress-${i}`, 'hello-service', ['yarn jest'])
+    runTest(`cypress-${i}`, `Cypress ${i}`, job)
+  }
 })
