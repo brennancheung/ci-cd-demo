47851ac2309cf5ee82b7de7e59f361367024e463
Brennan Cheung <git@brennancheung.com>
2020-01-23 16:33:34 -0800

    unique names

--- brigade.js
+++ brigade.js
@@ -2,49 +2,49 @@ const { events, Job } = require('brigadier')
 
 const registry = 'core.harbor.volgenic.com/ui'
 
-const sendCheckStatus = (e, summary = 'Beginning test run', conclusion, text) => {
-  console.log('Sending check status')
-  // This Check Run image handles updating GitHub
-  // const checkRunImage = 'brigadecore/brigade-github-check-run:latest'
-
-  // This image was pulled from Docker Hub and added to the local private registry to avoid
-  // network latency and to make tests run faster.
-  const checkRunImage = `${registry}/report-check-status`
-
-  const job = new Job('start-run', checkRunImage)
-  const env = {
-    CHECK_PAYLOAD: e.payload,
-    CHECK_NAME: 'tests',
-    CHECK_TITLE: 'Tests',
-    CHECK_SUMMARY: summary,
-  }
-  if (conclusion) env.CHECK_CONCLUSION = conclusion
-  if (text) env.CHECK_TEXT = text
-  job.imageForcePull = true
-  job.env = env
-  job.streamLogs = false
-  job.imagePullSecrets = ['regcred']
-  return job.run()
-}
-
-const createJob = (name, image, tasks = []) => {
-  const fullImage = `${registry}/${image}`
-  const job = new Job(name, fullImage, tasks, true)
-  job.streamLogs = true
-  job.imagePullSecrets = ['regcred']
-  job.imageForcePull = true
-  return job
-}
 
 events.on('check_suite:requested', (e, project) => {
+  const sendCheckStatus = (jobName, summary = 'Beginning test run', conclusion, text) => {
+    console.log('Sending check status')
+    // This Check Run image handles updating GitHub
+    // const checkRunImage = 'brigadecore/brigade-github-check-run:latest'
+
+    // This image was pulled from Docker Hub and added to the local private registry to avoid
+    // network latency and to make tests run faster.
+    const checkRunImage = `${registry}/report-check-status`
+
+    const job = new Job(jobName, checkRunImage)
+    const env = {
+      CHECK_PAYLOAD: e.payload,
+      CHECK_NAME: 'tests',
+      CHECK_TITLE: 'Tests',
+      CHECK_SUMMARY: summary,
+    }
+    if (conclusion) env.CHECK_CONCLUSION = conclusion
+    if (text) env.CHECK_TEXT = text
+    job.imageForcePull = true
+    job.env = env
+    job.streamLogs = false
+    job.imagePullSecrets = ['regcred']
+    return job.run()
+  }
+
+  const createJob = (name, image, tasks = []) => {
+    const fullImage = `${registry}/${image}`
+    const job = new Job(name, fullImage, tasks, true)
+    job.streamLogs = true
+    job.imagePullSecrets = ['regcred']
+    job.imageForcePull = true
+    return job
+  }
   // Webhook event received
   console.log(`Received check_suite for commit ${e.revision.commit}`)
 
   // Run unit tests
   console.log('About to run unit tests')
-  sendCheckStatus(e)
+  sendCheckStatus('startRun')
 
   createJob('jest-runner', 'hello-service', ['yarn jest']).run()
-    .then(result => sendCheckStatus(e, 'Tests passed', 'success', result.toString()))
-    .catch(err => sendCheckStatus(e, 'Tests failed', 'failure', err))
+    .then(result => sendCheckStatus('jestResults', 'Tests passed', 'success', result.toString()))
+    .catch(err => sendCheckStatus('jestResults', 'Tests failed', 'failure', err))
 })
