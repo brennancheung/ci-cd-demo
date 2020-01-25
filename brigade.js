a04894db88d187b148ccf5abaf0cc7da43c6cb0e
Brennan Cheung <git@brennancheung.com>
2020-01-24 19:18:02 -0800

    cleanup

--- brigade.js
+++ brigade.js
@@ -2,6 +2,7 @@ const { events, Job } = require('brigadier')
 const { helpers } = require('./brigade/helpers')
 
 events.on('check_suite:requested', async (e, project) => {
+  // Create helpers with their context bound to the event and project.
   const { createJob, runTest } = helpers(e, project)
 
   const lintJob = createJob('lint-runner', 'hello-service', ['yarn jest'])
@@ -9,12 +10,4 @@ events.on('check_suite:requested', async (e, project) => {
 
   const unitTestsJob = createJob('unit-runner', 'hello-service', ['yarn jest'])
   runTest('unit', 'Unit', unitTestsJob)
-
-  /*
-  const maxParallelIntegration = 2
-  for (let i=1; i<=maxParallelIntegration; i++) {
-    const job = createJob(`cypress-${i}`, 'hello-service', ['yarn jest'])
-    runTest(`cypress-${i}`, `Cypress ${i}`, job)
-  }
-  */
 })
